987abe9f324c080f52e6d9fb9622cb8ce689e4fd
GrayingOut <oliverredstone05@gmail.com>
2022-12-01 22:04:01 +0000

    Advent of Code 2015 - Day 25

--- 2015/day25/Main.java
+++ 2015/day25/Main.java
@@ -1,5 +1,5 @@
 
-public class Main {
+public final class Main {
 
     private static final int startCode = 20151125;
     private static final int multiplier = 252533;
@@ -9,36 +9,47 @@ public class Main {
     private static final int goalCol = 3083;
 
     public static void main(String[] args) {
+        long startTime = System.nanoTime();
+
+        /* Initial code */
         long currentCode = startCode;
         
+        /* Current row and col start positions */
         int row = 2;
         int col = 1;
 
         boolean found = false;
 
+        /* Brute force the code until at row and col */
         while (!found) {
+            /* Temp row for allowing manipulation */
             int tmpRow = row;
             col = 1;
 
+            /* Repeats until reaches first row */
             while (tmpRow >= 1) {
-                if (tmpRow == goalRow && col == goalCol) {
-                    System.out.printf("%s,%s\n", tmpRow, col);
-                }
-
+                /* Calculate the next code */
                 currentCode = (currentCode * multiplier) % divisor;
 
+                /* Check if at target row and col */
                 if (tmpRow == goalRow && col == goalCol) {
                     found = true;
                     break;
                 }
 
+                /* Decrement row and increment col (moves to next grid cell) */
                 col++;
                 tmpRow--;
             }
 
+            /* Update the start row */
             row++;
         }
 
-        System.out.println(currentCode);
+        long timeTaken = System.nanoTime() - startTime;
+
+        /* Output result */
+        System.out.printf("Time: %sms\n", timeTaken/1_000_000);
+        System.out.printf("Code: %s\n", currentCode);
     }
 }
