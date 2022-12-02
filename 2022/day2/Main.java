dc641c76582f8ec8325b1e9690785cabdab23386
GrayingOut <oliverredstone05@gmail.com>
2022-12-02 08:30:37 +0000

    Advent of Code 2022 - Day 2 (Optimised)

--- 2022/day2/Main.java
+++ 2022/day2/Main.java
@@ -4,77 +4,115 @@ import java.util.List;
 
 public final class Main {
 
+    private static List<String> lines;
+
     public static void main(String[] args) throws Exception {
         long startTime = System.nanoTime();
 
         /* Open file and read lines */
         File file = new File("data.txt");
-        List<String> lines = Files.readAllLines(file.toPath());
+        lines = Files.readAllLines(file.toPath());
 
-        int score = 0;
-
-        for (String line : lines) {
-            /* Get score of play */
-            int opponentPlayScore = line.charAt(0)-64;
-            int gameEnd = line.charAt(2) - 87;
-            int ourPlayScore = 0;
-
-            /* Calculate our play */
-            switch (gameEnd) {
-                case 1: {
-                    /* Need a lose */
-                    ourPlayScore = opponentPlayScore == 1 ? 3 : opponentPlayScore - 1;
-                    break;
-                }
-                case 2: {
-                    /* Need a draw */
-                    ourPlayScore = opponentPlayScore;
-                    break;
-                }
-                case 3: {
-                    /* Need a win */
-                    ourPlayScore = opponentPlayScore == 3 ? 1 : opponentPlayScore + 1;
-                }
-            }
-
-            score += ourPlayScore;
-
-            /* Rock - Scissors */
-            if (opponentPlayScore == 1 && ourPlayScore == 3) {
-                /* Lose */
-                score += 0;
-                continue;
-            }
-
-            /* Scissors - Rock */
-            if (opponentPlayScore == 3 && ourPlayScore == 1) {
-                /* Win */
-                score += 6;
-                continue;
-            }
-            
-            /* Rock - Rock, Paper - Paper, Scissors - Scissors */
-            if (opponentPlayScore == ourPlayScore) {
-                /* Draw */
-                score += 3;
-                continue;
-            }
-            
-            /* Rock - Paper, Paper - Scissors */
-            if (opponentPlayScore < ourPlayScore) {
-                /* Win */
-                score += 6;
-                continue;
-            }
-            
-            /* Lose */
-            score += 0;
-        }
+        int part1Score = part1();
+        int part2Score = part2();
 
         long timeTaken = System.nanoTime() - startTime;
 
         /* Output results */
         System.out.printf("Time: %sms\n", timeTaken/1_000_000);
-        System.out.printf("Score: %s\n", score);
+        System.out.printf("Part 1 Score: %s\n", part1Score);
+        System.out.printf("Part 2 Score: %s\n", part2Score);
+    }
+
+    /**
+     * Part 1 of the problem
+     * 
+     * @return The solution
+     */
+    private static final int part1() {
+        int score = 0;
+        
+        for (String line : lines) {
+            /* Get score of play */
+            int opponentPlay = line.charAt(0)-64;
+            int ourPlay = line.charAt(2) - 87;
+
+            score += ourPlay;
+            score += getGameScore(opponentPlay, ourPlay);
+        }
+
+        return score;
+    }
+
+    /**
+     * Part 2 of the problem
+     * 
+     * @return The solution
+     */
+    private static final int part2() {
+        int score = 0;
+        
+        for (String line : lines) {
+            /* Get score of play */
+            int opponentPlay = line.charAt(0)-64;
+            int gameEnd = line.charAt(2) - 87;
+            int ourPlay = getOurPlay(opponentPlay, gameEnd);
+
+            score += ourPlay;
+            score += getGameScore(opponentPlay, ourPlay);
+        }
+
+        return score;
+    }
+
+    /**
+     * Gets the play we need to play to end the game
+     * as needed
+     * 
+     * @param opponentPlay The opponent's play
+     * @param gameEnd      The needed game end
+     * @return Our play
+     */
+    private static final int getOurPlay(int opponentPlay, int gameEnd) {
+        switch (gameEnd) {
+            case 1: {
+                /* Need a lose */
+                return opponentPlay == 1 ? 3 : opponentPlay - 1;
+            }
+            case 2: {
+                /* Need a draw */
+                return opponentPlay;
+            }
+            case 3: {
+                /* Need a win */
+                return opponentPlay == 3 ? 1 : opponentPlay + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    /**
+     * Returns the score of the game
+     * 
+     * @param opponentPlay The play of the opponent
+     * @param ourPlay      Our play
+     * @return The game score
+     */
+    private static final int getGameScore(int opponentPlay, int ourPlay) {
+        /* Rock - Rock, Paper - Paper, Scissors - Scissors */
+        if (opponentPlay == ourPlay) {
+            /* Draw */
+            return 3;
+        }
+
+        /* Rock - Paper, Paper - Scissors, Scissors - Rock */
+        if (!(opponentPlay == 1 && ourPlay == 3) && (opponentPlay < ourPlay || opponentPlay == 3 && ourPlay == 1)) {
+            /* Win */
+            return 6;
+        }
+
+        /* Paper - Rock, Rock - Scissors, Scissors - Paper */
+        return 0;
     }
 }
--- 2022/day2/NOTES.md
+++ 2022/day2/NOTES.md
@@ -2,4 +2,4 @@
 
 Initially fought of using a HashMap for look up of values, but then remembered ASCII values - just needed the right value to subtract. The can also be used to determine the outcome. For the second part, a simple switch statement with some ternaries to determine the play - as it needs to wrap around.
 
-Takes around 10ms
+Takes around 2ms
